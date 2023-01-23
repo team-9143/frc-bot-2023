cbf3e08c0861e1e87b84cdaf63409efd0eb5a593
TheTofuMan <48603684+TheTofuMan@users.noreply.github.com>
2023-01-23 15:52:25 -0800

    Controller test

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -6,6 +6,5 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class OI {
     public final static Joystick m_stick = new Joystick(0);
-    public final static JoystickButton m_button2 = new JoystickButton(m_stick, 2);
-    public final static JoystickButton m_button1 = new JoystickButton(m_stick, 1);
+    public final static LogitechController logitechController = new LogitechController(m_stick);
 }
