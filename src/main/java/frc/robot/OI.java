1b1fae8be4ac4257e92371bd768111e8f64a6a69
Sid Thomas <thomas.siddharth@gmail.com>
2023-01-26 13:44:02 -0800

    rm unnecessary lines

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,10 +1,5 @@
 package frc.robot;
 
-// import edu.wpi.first.wpilibj.GenericHID;
-// import edu.wpi.first.wpilibj.Joystick;
-// import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-
 public class OI {
-    // public final static Joystick m_stick = new Joystick(0);
     public final static LogitechController m_controller = new LogitechController(0);
 }
