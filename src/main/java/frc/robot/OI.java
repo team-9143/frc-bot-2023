4b4f5c210183c1564258c1a5a11747cd08cf1530
Sid Thomas <thomas.siddharth@gmail.com>
2023-02-06 19:30:57 -0800

    trigger for B button to reset gyro to current position

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,8 +1,28 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class OI {
-    public final static LogitechController m_controller = new LogitechController(0);
-    public static ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+  public final static LogitechController m_controller = new LogitechController(0);
+  public final static ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+
+  private final static JoystickButton b_button = new JoystickButton(m_controller, LogitechController.BTN_B);
+
+  public OI() {
+    b_button.onTrue(new ResetGyro());
+  }
+
+  private class ResetGyro extends CommandBase {
+    @Override
+    public void initialize() {
+      gyro.reset();
+    }
+
+    @Override
+    public boolean isFinished() {
+      return true;
+    }
+  }
 }
