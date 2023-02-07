2977c45ceb889de4e9456397a94dfd4707641f0b
Sid Thomas <thomas.siddharth@gmail.com>
2023-02-06 19:42:05 -0800

    remove unnecessary end() method in drive command

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -40,10 +40,6 @@ public class Drive extends CommandBase {
     }
   }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
