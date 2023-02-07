7646af6279f08df42c9926bc7074cc4c2e0595af
Sid Thomas <thomas.siddharth@gmail.com>
2023-02-07 09:34:01 -0800

    refactor gyro reset command to build off of the InstantCommand class

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,7 +1,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class OI {
@@ -11,16 +11,10 @@ public class OI {
   private final static JoystickButton b_button = new JoystickButton(m_controller, LogitechController.BTN_B);
 
   public OI() {
-    b_button.onTrue(new CommandBase() {
-      @Override
-      public void initialize() {
+    b_button.onTrue(new InstantCommand(new Runnable() {
+      public void run() {
         gyro.reset();
       }
-
-      @Override
-      public boolean isFinished() {
-        return true;
-      }
-    });
+    }));
   }
 }
