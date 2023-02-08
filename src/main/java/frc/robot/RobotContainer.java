665f266edc9aa74da5aeea89f0902e389f8f7196
Sid Thomas <thomas.siddharth@gmail.com>
2023-02-08 10:18:26 -0800

    add subsystem and trigger  initialization to robotcontainer class; rename motorcontrollergroup variables from sparks to motors

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,16 +4,16 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.*;
 import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.commands.TurnToAngle;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -24,15 +24,13 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Drivetrain sDrivetrain = new Drivetrain();
 
-  public final static MotorControllerGroup l_sparks = new MotorControllerGroup(new Spark(0), new Spark(1));
-  public final static MotorControllerGroup r_sparks = new MotorControllerGroup(new Spark(3), new Spark(4));
-  public final static DifferentialDrive m_robotDrive = new DifferentialDrive(r_sparks, l_sparks);
-
-
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final TurnToAngle cTurnToAngle = new TurnToAngle(sDrivetrain);
+  
+  public final static MotorControllerGroup l_motors = new MotorControllerGroup(new Spark(0), new Spark(1));
+  public final static MotorControllerGroup r_motors = new MotorControllerGroup(new Spark(3), new Spark(4));
+  public final static DifferentialDrive m_robotDrive = new DifferentialDrive(r_motors, l_motors);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -50,13 +48,8 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    new JoystickButton(OI.m_controller, LogitechController.BTN_B)
+      .onTrue(new InstantCommand(() -> OI.gyro.reset()));
   }
 
   /**
