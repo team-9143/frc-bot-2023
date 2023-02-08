7051815b9f203e80880ed5e9baef355bcb938919
Sid Thomas <thomas.siddharth@gmail.com>
2023-02-08 10:19:22 -0800

    move triggers from oi to robotcontainer

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,21 +1,8 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class OI {
   public final static LogitechController m_controller = new LogitechController(0);
   public final static ADXRS450_Gyro gyro = new ADXRS450_Gyro();
-
-  private final static JoystickButton b_button = new JoystickButton(m_controller, LogitechController.BTN_B);
-
-  public OI() {
-    b_button.onTrue(new InstantCommand(new Runnable() {
-      public void run() {
-        System.out.println("gyro reset");
-        gyro.reset();
-      }
-    }));
-  }
 }
