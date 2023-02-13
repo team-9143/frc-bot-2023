8aa3831b454820491223f93fc9a62721ceb6cddb
Sid Thomas <thomas.siddharth@gmail.com>
2023-02-13 10:44:31 -0800

    getter methods instead of public variables for limelight data

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -7,31 +7,17 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.OI;
 import edu.wpi.first.networktables.NetworkTableEntry;
-import frc.robot.commands.TargetTape;
 
 public class Limelight extends SubsystemBase {
-  private static final NetworkTableEntry
+  private final NetworkTableEntry
     tv = OI.limelight.getEntry("tv"),
     tx = OI.limelight.getEntry("tx"),
     ty = OI.limelight.getEntry("ty"),
     ta = OI.limelight.getEntry("ta"),
     ledMode = OI.limelight.getEntry("ledMode");
 
-  public static boolean valid;
-  public static double x, y, area;
-
   /** Creates a new Limelight. */
   public Limelight() {}
-
-  @Override
-  public void periodic() {
-    // Update variables
-    valid = (tv.getDouble(0) == 1) ? true : false;
-    x = tx.getDouble(0);
-    y = ty.getDouble(0);
-    area = ta.getDouble(0);
-    // TODO: Add AprilTag and 3D space related variables
-  }
 
   /**
    * Sets the limelight's LED mode.
@@ -45,11 +31,12 @@ public class Limelight extends SubsystemBase {
    * 
    * @param mode
    */
-  public void setLedMode(int mode) {
-    ledMode.setNumber(mode);
-  }
+  public void setLedMode(int mode) {ledMode.setNumber(mode);}
 
-  public int getLedMode() {
-    return (int) ledMode.getInteger(0);
-  }
+  public double getTx() {return tx.getDouble(0);}
+  public double getTy() {return ty.getDouble(0);}
+  public double getArea() {return ta.getDouble(0);}
+  public boolean getValid() {return (tv.getInteger(0) == 1) ? true : false;}
+  // TODO: Add AprilTag and 3D space related variables
+  public int getLedMode() {return (int) ledMode.getInteger(0);}
 }
