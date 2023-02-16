6f36e4764daa651c0174e0c785f4e6c9dbbf0540
Sid Thomas <thomas.siddharth@gmail.com>
2023-02-15 20:20:06 -0800

    refactor drivetrain to efficiently check for right stick

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -17,12 +17,7 @@ public class Drivetrain extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // Adjust and round right stick input
     double rs_X = OI.driver_cntlr.getRightStick()[0], rs_Y = OI.driver_cntlr.getRightStick()[1];
-    if (Math.abs(rs_X) < 0.3 && Math.abs(rs_Y) < 0.3) {
-      rs_X = 0;
-      rs_Y = 0;
-    }
 
     // Find angle and schedule TurnToAngle command
     if (OI.driver_cntlr.getPOV() != -1) {
@@ -30,7 +25,7 @@ public class Drivetrain extends SubsystemBase {
       heading = 45 * Math.round((float) OI.driver_cntlr.getPOV() / 45);
       heading = (heading == 360) ? 0 : heading;
       cTurnToAngle.setHeading(heading);
-    } else if (rs_X != 0 || rs_Y != 0) {
+    } else if (Math.abs(rs_X) > 0.3 || Math.abs(rs_Y) > 0.3) {
       // Find specific angle, input from right stick
       heading = Math.toDegrees(Math.atan2(rs_X, rs_Y));
       heading += (heading < 0) ? 360 : 0;
