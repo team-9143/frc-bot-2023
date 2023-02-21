c68211892ad2b0e49748f21d5213ad93923849b2
Sid Thomas <thomas.siddharth@gmail.com>
2023-02-21 15:17:45 -0800

    [feat] Initialize encoders

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -11,11 +11,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.commands.Drive;
 
 public class Drivetrain extends SubsystemBase {  
+  // Initialize motors, encoders, and differential drive
   private static final CANSparkMax
     fl_motor = new CANSparkMax(DrivetrainConstants.kFrontLeftDeviceID, MotorType.kBrushless),
     bl_motor = new CANSparkMax(DrivetrainConstants.kBackLeftDeviceID, MotorType.kBrushless),
     fr_motor = new CANSparkMax(DrivetrainConstants.kFrontRightDeviceID, MotorType.kBrushless),
     br_motor = new CANSparkMax(DrivetrainConstants.kBackRightDeviceID, MotorType.kBrushless);
+  
+  public final RelativeEncoder
+    fl_encoder = fl_motor.getEncoder(),
+    bl_encoder = bl_motor.getEncoder(),
+    fr_encoder = fr_motor.getEncoder(),
+    br_encoder = br_motor.getEncoder();
   
   public final DifferentialDrive robotDrive = new DifferentialDrive(
     new MotorControllerGroup(fl_motor, bl_motor),
