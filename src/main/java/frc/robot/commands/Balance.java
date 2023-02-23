9d8229fd12ba3ceb6051e28bef2cc21b98a877af
Sid Thomas <thomas.siddharth@gmail.com>
2023-02-23 10:19:44 -0800

    [feat] Initial balancing code

--- src/main/java/frc/robot/commands/Balance.java
+++ src/main/java/frc/robot/commands/Balance.java
@@ -11,10 +11,12 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class Balance extends CommandBase {
   private final Drivetrain drivetrain;
+  private double previousPitch;
   
   public Balance(Drivetrain drivetrain) {
     this.drivetrain = drivetrain;
-    
+    previousPitch = OI.pigeon.getPitch();
+
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(drivetrain);
   }
@@ -22,7 +24,17 @@ public class Balance extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    double pitch = OI.pigeon.getPitch();
     // TODO: move forward while pitch is positive, backward while pitch is negative; remember that pitch changes very quickly and the robot should stop at any small change
+    if (Math.abs(pitch) > DrivetrainConstants.kPitchDeadspot) {
+      if (Math.abs(pitch - previousPitch) > 1) {
+        drivetrain.stop();
+      } else {
+        drivetrain.robotDrive.arcadeDrive(0, DrivetrainConstants.kSpeedMult * 0.2, false);
+      }
+    } else {
+      cancel();
+    }
   }
 
   // Called once the command ends or is interrupted.
