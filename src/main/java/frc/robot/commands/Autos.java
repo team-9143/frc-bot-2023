2fca2e771913dc83046d8eb12bb80405a1935363
Sid Thomas <thomas.siddharth@gmail.com>
2023-03-09 15:30:59 -0800

    [feat] Full auton draft

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -12,19 +12,35 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 public final class Autos {
-  // Auto to score a pre-loaded cube, then pick up and score a cube from in front of the community
+  // Auto to score a pre-loaded cube, then pick up a cube from in front of the community and return
   public static final SequentialCommandGroup SideAuto(DriveDistance driveDistance, TurnToAngle turnToAngle, Intake intake, Command outtake) {
     return new SequentialCommandGroup(
       new ParallelDeadlineGroup(new WaitCommand(1), outtake),
-      new InstantCommand(() -> driveDistance.setDistance(24)),
-      driveDistance
+
+      new InstantCommand(() -> turnToAngle.setHeading(180)), turnToAngle,
+
+      new InstantCommand(() -> driveDistance.setDistance(48)), driveDistance,
+
+      new ParallelDeadlineGroup(new WaitCommand(1), intake),
+
+      new InstantCommand(() -> turnToAngle.setHeading(180)), turnToAngle,
+
+      new InstantCommand(() -> driveDistance.setDistance(36)), driveDistance
     );
   }
 
   // Auto to score a pre-loaded cube, drive over the charge station, then drive back and balance
-  public static final SequentialCommandGroup CenterAuto(Balance balance, DriveDistance driveDistance, TurnToAngle turnToAngle, Command ottake) {
+  public static final SequentialCommandGroup CenterAuto(Balance balance, DriveDistance driveDistance, TurnToAngle turnToAngle, Command outtake) {
     return new SequentialCommandGroup(
+      new ParallelDeadlineGroup(new WaitCommand(1), outtake),
 
+      new InstantCommand(() -> turnToAngle.setHeading(180)), turnToAngle,
+
+      new InstantCommand(() -> driveDistance.setDistance(48)), driveDistance,
+
+      new InstantCommand(() -> turnToAngle.setHeading(180)), turnToAngle,
+
+      balance
     );
   }
 }
