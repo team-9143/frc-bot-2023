256e1a85d3c568a39b8aa337b48f47f1d79dff5f
Sid Thomas <thomas.siddharth@gmail.com>
2023-03-10 22:04:52 -0800

    [feat] Add general-purpose controller class under OI

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -5,6 +5,7 @@ import frc.robot.Constants.DeviceConstants;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import com.ctre.phoenix.sensors.Pigeon2;
+import edu.wpi.first.wpilibj.GenericHID;
 
 public class OI {
   public final static LogitechController driver_cntlr = new LogitechController(DeviceConstants.kDriverCntlrPort);
@@ -13,4 +14,57 @@ public class OI {
   // In proper orientation, Pigeon is flat and facing so that X-axis is forward
   // Roll increases to the right, pitch to the front, and yaw counter-clockwise
   public final static Pigeon2 pigeon = new Pigeon2(DeviceConstants.kPigeonCANid);
+
+  // TODO: Migrate all controller callings to here
+  public static class Controller extends GenericHID {
+    public enum btn {
+      A(1),
+      B(2),
+      X(3),
+      Y(4),
+      LB(5),
+      RB(6),
+      LStick(9),
+      RStick(10),
+      Back(7),
+      Start(8);
+
+      public final int val;
+
+      btn (int val) {
+        this.val = val;
+      }
+    }
+
+    public enum axis {
+      leftX(0),
+      leftY(1),
+      rightX(4),
+      rightY(5),
+      leftTrigger(2),
+      rightTrigger(3);
+
+      public final int val;
+
+      axis (int val) {
+        this.val = val;
+      }
+    }
+
+    public Controller(int port) {
+      super(port);
+    }
+
+    /**
+     * @return The trigger value between -1 and 1, left being subtractive and right being additive
+     */
+    public double getTriggerButtons() {
+      return getRawAxis(axis.rightTrigger.val) - getRawAxis(axis.leftTrigger.val);
+    }
+
+    public double getLeftX() {return getRawAxis(axis.leftX.val);}
+    public double getLeftY() {return getRawAxis(axis.leftY.val);}
+    public double getRightX() {return getRawAxis(axis.rightX.val);}
+    public double getRightY() {return getRawAxis(axis.rightY.val);}
+  }
 }
