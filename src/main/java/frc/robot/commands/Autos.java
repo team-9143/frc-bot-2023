009185ed5c8cf749a6fb22c590e639d17e013914
Sid Thomas <thomas.siddharth@gmail.com>
2023-03-15 17:27:52 -0700

    [feat] Add balance auto

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -4,13 +4,16 @@
 
 package frc.robot.commands;
 
+import frc.robot.OI;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.DrivetrainConstants;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.IntakeTilt;
 import frc.robot.subsystems.IntakeWheels;
-import edu.wpi.first.wpilibj2.command.Command;
 
 public final class Autos {
   public static enum Type {
@@ -56,9 +59,36 @@ public final class Autos {
     return new SequentialCommandGroup(
       sIntakeWheels.getOuttakeCommand().withTimeout(1),
 
-      new DriveDistance(sDrivetrain).beforeStarting(() -> DriveDistance.setDistance(-48)),
+      // Move back until pitch is greater than 10
+      new FunctionalCommand(
+        () -> sDrivetrain.moveStraight(-0.25),
+        () -> {},
+        interrupted -> sDrivetrain.stop(),
+        () -> OI.pigeon.getPitch() > 10,
+        sDrivetrain
+      ),
 
-      new DriveDistance(sDrivetrain).beforeStarting(() -> DriveDistance.setDistance(24)),
+      // Move back until pitch is less than -10
+      new FunctionalCommand(
+        () -> sDrivetrain.moveStraight(-0.25),
+        () -> {},
+        interrupted -> sDrivetrain.stop(),
+        () -> OI.pigeon.getPitch() < -10,
+        sDrivetrain
+      ),
+
+      // Move back until pitch is close to flat
+      new FunctionalCommand(
+        () -> sDrivetrain.moveStraight(-0.25),
+        () -> {},
+        interrupted -> sDrivetrain.stop(),
+        () -> Math.abs(OI.pigeon.getPitch()) < DrivetrainConstants.kBalanceTolerance,
+        sDrivetrain
+      ),
+
+      new DriveDistance(sDrivetrain).beforeStarting(() -> DriveDistance.setDistance(-5)),
+
+      new DriveDistance(sDrivetrain).beforeStarting(() -> DriveDistance.setDistance(16)),
 
       new Balance(sDrivetrain)
     );
