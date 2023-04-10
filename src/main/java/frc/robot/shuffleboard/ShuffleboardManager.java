4c5c6cf223b75a6c55b4c601861d3893f0501928
Sid Thomas <thomas.siddharth@gmail.com>
2023-04-10 11:25:12 -0700

    [feat] Create ShuffleboardManager

---
+++ src/main/java/frc/robot/shuffleboard/ShuffleboardManager.java
@@ -0,0 +1,33 @@
+package frc.robot.shuffleboard;
+
+import java.util.ArrayList;
+
+/** Initializes Shuffleboard */
+public class ShuffleboardManager {
+  private static ShuffleboardManager m_instance;
+
+  /** Returns the singleton instance. */
+  public static ShuffleboardManager getInstance() {
+    if (m_instance == null) {
+      m_instance = new ShuffleboardManager();
+    }
+    return m_instance;
+  }
+
+  public interface ShuffleboardTabBase {
+    abstract void initialize();
+  }
+
+  private static final boolean m_debugging = false;
+
+  private static final ArrayList<ShuffleboardTabBase> m_tabs = new ArrayList<>();
+
+  private ShuffleboardManager() {
+    m_tabs.add(null);
+    if (m_debugging) {
+      m_tabs.add(null);
+    }
+
+    m_tabs.forEach(e -> e.initialize());
+  }
+}
