64e8eb62933565f48aaabed5cc026a28e0f8b8c8
Sid <thomas.siddharth@gmail.com>
2023-04-14 11:39:32 -0700

    [fix] Initialize autonomous command as empty InstantCommand

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 import frc.robot.autos.AutoSelector;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 import frc.robot.subsystems.IntakeTilt;
 import frc.robot.commands.TurnToAngle;
@@ -23,7 +24,7 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
  * project.
  */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+  private Command m_autonomousCommand = new InstantCommand();
 
   @Override
   public void robotInit() {
