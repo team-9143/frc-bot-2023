68e047742e67d3142318d3a5b4c4bedd17492965
Sid Thomas <thomas.siddharth@gmail.com>
2023-04-14 13:06:10 -0700

    [feat] Remove unnecessary parts of Ending

--- src/main/java/frc/robot/autos/Endings.java
+++ src/main/java/frc/robot/autos/Endings.java
@@ -4,8 +4,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
-import frc.robot.subsystems.IntakeWheels;
-
 import frc.robot.commands.TurnToAngle;
 
 /** Contains auton endings. */
@@ -16,20 +14,9 @@ public class Endings {
       case TURN_AWAY:
         // Turn to face away from the drive station
         return new TurnToAngle(180);
-
       case TURN_CLOSE:
         // Turn to face the drive station
         return new TurnToAngle(0);
-
-      case RETURN_FROM_CONE:
-        // If picking up a cone, turn and return to the grid
-        if (body == AutoSelector.Body.PICKUP_CONE) {
-          return new SequentialCommandGroup(
-            new TurnToAngle(0),
-            new DriveDistance(205)
-          );
-        }
-
       default:
         return new InstantCommand();
     }
